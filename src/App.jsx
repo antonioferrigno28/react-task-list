4df6c0da9db4283cc4f9437e2f53ec9f222b4916
antonioferrigno28 <ferrigno1@hotmail.it>
2024-11-27 15:31:11 +0100

    Array moved to data folder, no bonus

--- src/App.jsx
+++ src/App.jsx
@@ -7,89 +7,54 @@
 // Buon lavoro!
 
 import { useState } from "react";
+import { tasks } from "./data/tasksData";
 
 import "./App.css";
 
 function App() {
-  const tasks = [
-    {
-      id: 1,
-      title: "Implementare la homepage",
-      priority: 1,
-      estimatedTime: 120,
-      state: "completed",
-    },
-    {
-      id: 2,
-      title: "Sviluppare il menu di navigazione",
-      priority: 2,
-      estimatedTime: 60,
-      state: "completed",
-    },
-    {
-      id: 3,
-      title: "Creare il footer del sito",
-      priority: 3,
-      estimatedTime: 30,
-      state: "completed",
-    },
-    {
-      id: 4,
-      title: "Ottimizzare le performance",
-      priority: 1,
-      estimatedTime: 180,
-      state: "completed",
-    },
-    {
-      id: 5,
-      title: "Scrivere test per i componenti",
-      priority: 2,
-      estimatedTime: 90,
-      state: "completed",
-    },
-    {
-      id: 6,
-      title: "Implementare la pagina dei contatti",
-      priority: 3,
-      estimatedTime: 60,
-      state: "completed",
-    },
-    {
-      id: 7,
-      title: "Aggiungere animazioni CSS",
-      priority: 2,
-      estimatedTime: 30,
-      state: "backlog",
-    },
-    {
-      id: 8,
-      title: "Integrare l'API di autenticazione",
-      priority: 1,
-      estimatedTime: 120,
-      state: "in_progress",
-    },
-    {
-      id: 9,
-      title: "Aggiornare la documentazione",
-      priority: 3,
-      estimatedTime: 60,
-      state: "backlog",
-    },
-    {
-      id: 10,
-      title: "Rifattorizzare il codice CSS",
-      priority: 2,
-      estimatedTime: 90,
-      state: "in_progress",
-    },
-  ];
+  const backlogTasks = tasks.filter(
+    (task) => task.state === "backlog" || task.state === "in_progress"
+  );
+  const completedTasks = tasks.filter((task) => task.state === "completed");
 
-  console.log(tasks);
   return (
-    <>
-      <h1>ao</h1>
-      <h2>ao</h2>
-    </>
+    <div>
+      <h1 className=" bg-primary py-3 px-3">Task Manager</h1>
+      <div className=" px-3">
+        {/* Lista Backlog & In Progress */}
+        <div>
+          <h2 className="fs-5 fw-bold">
+            Current tasks ({backlogTasks.length})
+          </h2>
+
+          <ul className="list-unstyled">
+            {backlogTasks.map((task) => (
+              <li key={task.id} className="my-4">
+                <strong>{task.title}</strong> <span>({task.state})</span>
+                <div>Priority: {task.priority}</div>
+                <div>Estimated Time: {task.estimatedTime} min</div>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <hr />
+        {/* Lista Completed */}
+        <div>
+          <h2 className="fs-5 fw-bold">
+            Completed Tasks ({completedTasks.length})
+          </h2>
+          <ul className="list-unstyled">
+            {completedTasks.map((task) => (
+              <li key={task.id} className="my-4">
+                <strong>{task.title}</strong> <span>({task.state})</span>
+                <div>Priority: {task.priority}</div>
+                <div>Estimated Time: {task.estimatedTime} min</div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
 
