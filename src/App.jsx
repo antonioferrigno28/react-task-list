616b2e29fffb7378b8cd01f5ff9373c9112a1811
antonioferrigno28 <ferrigno1@hotmail.it>
2024-11-27 15:12:58 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,96 @@
+// Esercizio
+// Mostriamo in due liste diverse gli elementi dell’array tasks. Nella prima lista mostriamo solo i task (title, priority e estimatedTime) che hanno stato “backlog” o “in_progress”.
+// Nella seconda lista mostriamo i task che hanno stato “completed”.
+// Infine riprodurre il layout lasciato in allegato.
+// BONUS
+// Mostrare lo stato di ogni task in un piccolo tag di fianco al titolo, associando però ad ogni stato un colore diverso.
+// Buon lavoro!
+
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const tasks = [
+    {
+      id: 1,
+      title: "Implementare la homepage",
+      priority: 1,
+      estimatedTime: 120,
+      state: "completed",
+    },
+    {
+      id: 2,
+      title: "Sviluppare il menu di navigazione",
+      priority: 2,
+      estimatedTime: 60,
+      state: "completed",
+    },
+    {
+      id: 3,
+      title: "Creare il footer del sito",
+      priority: 3,
+      estimatedTime: 30,
+      state: "completed",
+    },
+    {
+      id: 4,
+      title: "Ottimizzare le performance",
+      priority: 1,
+      estimatedTime: 180,
+      state: "completed",
+    },
+    {
+      id: 5,
+      title: "Scrivere test per i componenti",
+      priority: 2,
+      estimatedTime: 90,
+      state: "completed",
+    },
+    {
+      id: 6,
+      title: "Implementare la pagina dei contatti",
+      priority: 3,
+      estimatedTime: 60,
+      state: "completed",
+    },
+    {
+      id: 7,
+      title: "Aggiungere animazioni CSS",
+      priority: 2,
+      estimatedTime: 30,
+      state: "backlog",
+    },
+    {
+      id: 8,
+      title: "Integrare l'API di autenticazione",
+      priority: 1,
+      estimatedTime: 120,
+      state: "in_progress",
+    },
+    {
+      id: 9,
+      title: "Aggiornare la documentazione",
+      priority: 3,
+      estimatedTime: 60,
+      state: "backlog",
+    },
+    {
+      id: 10,
+      title: "Rifattorizzare il codice CSS",
+      priority: 2,
+      estimatedTime: 90,
+      state: "in_progress",
+    },
+  ];
+
+  console.log(tasks);
+  return (
+    <>
+      <h1>ao</h1>
+      <h2>ao</h2>
+    </>
+  );
+}
+
+export default App;
